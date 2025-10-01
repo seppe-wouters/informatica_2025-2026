f1214e2c1418975cdab7dc971a84699164bfa71e
Seppe Wouters <seppe.wouters@sintmaartencampus.be>
2025-10-01 12:03:07 +0200

    qsdf

---
+++ ReizenApp/Business/Reis.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business
+{
+    public class Reis
+    {
+        private string _land;
+        private int _aantalDagen;
+        private decimal _kostprijs;
+
+        public Reis(string land, int aantalDagen, decimal kostprijs)
+        {
+            _land = land;
+            _aantalDagen = aantalDagen;
+            _kostprijs = kostprijs;
+        }
+
+        public string Land
+        {
+            get { return _land; }
+            set { _land = value; }
+        }
+        public int AantalDagen   
+        {
+            get { return _aantalDagen; }
+            set { _aantalDagen = value; }
+        }
+        public decimal Kostprijs
+        {
+            get { return _kostprijs; }
+            set { _kostprijs = value; }
+        }
+
+        public string Omschijving()
+        {
+            return $"{_aantalDagen.ToString()} dag/dagen naar {_land} voor {_kostprijs.ToString("C")}";
+        }
+    }
+}
