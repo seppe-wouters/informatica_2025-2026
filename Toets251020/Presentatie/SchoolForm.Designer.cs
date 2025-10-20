1a18487f338e97b085dc8a70ef702471fa78ce06
Seppe Wouters <seppe.wouters@sintmaartencampus.be>
2025-10-20 09:58:37 +0200

    Toets251020

--- Toets251020/Presentatie/SchoolForm.Designer.cs
+++ Toets251020/Presentatie/SchoolForm.Designer.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d1faee6b432cdfcd22ebdc34c5f500ae2917674e347e1490cfa9a1bc5a1e939d
-size 11800
+oid sha256:14c4df85f9e3ac2f954c4f50e8ca4d47d29aff82a9e041cfd918515bf3206d74
+size 12860
--- Toets251020/Presentatie/SchoolForm.cs
+++ Toets251020/Presentatie/SchoolForm.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:699fcf4edaf93b175251f02e502c1cd4828e51c3bf4c16754d450a8b2a89934a
-size 6295
+oid sha256:df6346eab046cfb8ba31ae0f6ea75ce9730bd9a1caa328a822329c18a7723c2e
+size 6437
